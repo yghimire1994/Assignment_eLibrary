9fa8256525cafecc24332501b2b0bab82a32e89d
Yubraj Ghimire <yghimire@gmail.com>
2019-08-07 12:41:41 -0500

    first commit

---
+++ src/main/java/edu/mum/cs/cs425/demos/elibrarydemocrudweb/repository/BookRepository.java
@@ -0,0 +1,21 @@
+package edu.mum.cs.cs425.demos.elibrarydemocrudweb.repository;
+
+import edu.mum.cs.cs425.demos.elibrarydemocrudweb.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Integer> {
+    // This interface definition relies on the public abstract methods
+    // inherited from the super interface, CrudRepository<T, ID>
+    // We may override any or add more methods here, if needed.
+    Optional<Book> findBookByIsbn(String isbn);
+    @Query(value = "SELECT b FROM Book b WHERE lower(b.title) like lower(concat('%', :keyword,'%') ) ")
+    List<Book> searchBook(@Param("keyword")String keyword);
+}
---
+++ src/main/java/edu/mum/cs/cs425/demos/elibrarydemocrudweb/service/BookService.java
@@ -0,0 +1,19 @@
+package edu.mum.cs.cs425.demos.elibrarydemocrudweb.service;
+
+import edu.mum.cs.cs425.demos.elibrarydemocrudweb.model.Book;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface BookService {
+
+    public abstract Iterable<Book> getAllBooks();
+    public abstract Page<Book> getAllBooksPaged(int pageNo);
+    public abstract Book saveBook(Book book);
+    public abstract Book getBookById(Integer bookId);
+    public abstract void deleteBookById(Integer bookId);
+    public abstract Optional<Book> findByISBN(String isbn);
+    public abstract List<Book> searchBook(String keyword);
+
+}
